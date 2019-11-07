35ae9048b94ccbe0e7ec5a74f4558ff1b6a299d0
Hikeniken <rotmghikenviken@gmail.com>
2019-11-06 18:38:14 -0600

    add starting content to cherry blue page

--- scripts.js
+++ scripts.js
@@ -23,8 +23,7 @@ window.addEventListener("DOMContentLoaded", function() {
   let boxes = document.querySelectorAll(".box");
 
   Array.from(boxes, function(box) {
-    box.addEventListener("click", function() {
-      alert(this.classList[1]);
+    box.addEventListener("click", function() ;
     });
   });
 });
