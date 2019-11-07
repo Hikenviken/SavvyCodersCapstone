4bab3f3e573305c28202fade40dc82def907c419
Hikeniken <rotmghikenviken@gmail.com>
2019-11-06 21:03:36 -0600

    add lots of changes to button and blue page

--- scripts.js
+++ scripts.js
@@ -23,7 +23,8 @@ window.addEventListener("DOMContentLoaded", function() {
   let boxes = document.querySelectorAll(".box");
 
   Array.from(boxes, function(box) {
-    box.addEventListener("click", function() ;
+    box.addEventListener("click", function() {
+      alert(this.classList[1]);
     });
   });
 });
